32015aaadce5ab46cc5c08484901d4ab90d837c1
Jonathan Hawk <jhawk@mindgrub.com>
2017-03-09 13:48:51 -0500

    Maybe final

--- start.sh
+++ start.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 set -e
 
-ls -l /etc/git-secret/ssh
 gitSecret="/etc/git-secret/ssh"
+if [ "$GIT_SYNC_PRIVATE_KEY" ]; then
+    mkdir -p /etc/git-secret
+    echo $GIT_SYNC_PRIVATE_KEY > "$gitSecret"
+fi
+
 if [ -f "$gitSecret" ]; then
     chmod 400 "$gitSecret"
     chown $(id -u):$(id -g) "$gitSecret"
 fi
-ls -la /etc/git-secret
 if [ -d "/git" ]; then
     rm -rf /git/*
 fi
