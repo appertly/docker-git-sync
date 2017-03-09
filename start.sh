67d817cf1ca6744d8e2aa213cf8734f5cd4261f6
Jonathan Hawk <jhawk@mindgrub.com>
2017-03-09 12:51:52 -0500

    Directory fix

--- start.sh
+++ start.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-#set -e
-
-echo id -u
-ls -l /etc/git-secret/ssh
+set -e
 
 gitSecret="/etc/git-secret/ssh"
 if [ -f "$gitSecret" ]; then
     chmod 400 "$gitSecret"
     chown $(id -u):$(id -g) "$gitSecret"
+fi
+if [ -d "/git" ]; then
+    rm -rf /git/*
 fi
 
 /git-sync "$@"
